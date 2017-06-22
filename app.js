61a2b68901bd3f265a868d74b8327ddf8f6a4a0c
Rob Brentnall <rob.brentnall@gmail.com>
2017-06-22 13:15:07 +0000

    controllers and city service set up

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
-var weatherForecast = angular.module('weatherForecast', ['ngRoute', 'ngResource']);
-
-weatherForecast.config(function($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'pages/home.html',
-            controller: 'homeController'
-        })
-        .when('/forecast', {
-            templateUrl: 'pages/forecast.html',
-            controller: 'forecastController'
-        })
-});
-
-weatherForecast.controller('homeController', ['$scope', function($scope) {
-    $scope.title = "Home";
-}]);
-
-weatherForecast.controller('forecastController', ['$scope', function($scope) {
-    $scope.title = "Forecast";
-}]);
+(function(){
+    
+    var weatherForecast = angular.module('weatherForecast', ['ngRoute', 'ngResource']);
+    
+    weatherForecast.config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'pages/home.html',
+                controller: 'homeController'
+            })
+            .when('/forecast', {
+                templateUrl: 'pages/forecast.html',
+                controller: 'forecastController'
+            })
+    });
+    
+    weatherForecast.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+        $scope.setCity = function() {
+            cityService.setCity($scope.city);
+        };
+    }]);
+    
+    weatherForecast.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+        $scope.city = cityService.getCity();
+    }]);
+    
+    weatherForecast.service('cityService', function() {
+        var city = "London";
+        
+        return {
+            setCity: function(val) {
+                city = val || city;
+            },
+            getCity: function() {
+                return city;
+            }
+        }
+    });
+    
+})();
